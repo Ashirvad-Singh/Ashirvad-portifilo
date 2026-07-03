ea22a236103c7a65c1357786e0232486deacffac
Adat <ashirvad2912@gmail.com>
2026-07-03 11:09:49 +0530

    mobile issue corrected

--- src/components/ui/macbook-scroll.tsx
+++ src/components/ui/macbook-scroll.tsx
@@ -68,7 +68,7 @@ export const MacbookScroll = ({
   return (
     <div
       ref={ref}
-      className="flex min-h-[50vh] md:min-h-[120vh] shrink-0 scale-[0.45] transform flex-col items-center justify-start py-0 [perspective:800px] sm:scale-50 md:scale-100 md:pt-16 md:pb-20"
+      className="flex min-h-[100vh] md:min-h-[120vh] shrink-0 scale-[0.45] origin-top md:origin-center -mb-[450px] md:mb-0 transform flex-col items-center justify-start py-0 [perspective:800px] sm:scale-50 sm:-mb-[350px] md:scale-100 md:pt-16 md:pb-20"
     >
       <motion.h2
         style={{
